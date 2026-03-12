0b38aeb08930e7b5a9d3a497cd3eee55bfe580b8
Rhuan S. Wolfgramm <124512081+JSRuwen@users.noreply.github.com>
2026-03-12 10:24:53 -0300

    Create 20260312_bubbleSort-InsertionSort.c

---
+++ 20260312_bubbleSort-InsertionSort.c
@@ -0,0 +1,104 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+//char alfabeto = {'a','b','c','d','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+//char nomes = {"Gustavus","Dalston","Niko","Emile","Marieann","Wald","Lynsey","Ferdinand","Yolane","Monah","Cyrillus","Cherilyn","Robert","Robbyn","Stillmann","Rozanne","Bordie","Elissa","Kele","Rozanne"};
+
+int ordenacao(char *p) {
+    
+}
+
+
+
+
+void selectionSort(int *p, int n) {
+    int menor;
+    
+    for( int i = 0; i < n; i++) {
+        menor = i;
+    
+        for(int x = i + 1; x < n; x++) {
+            
+            
+            if( p[x] < p[menor]) {
+                int temp = p[menor];
+                p[menor] = p[x];
+                p[x] = temp;
+            }
+        }
+    }
+    
+    for (int i =0; i < n; i++) {
+        printf("%d ",p[i]);
+    }
+}
+
+void bubbleSort(int *v, int n) {
+    int swap;
+    
+    for(int i = 0; i < n; i++) {
+        swap = 0;
+        
+        for(int j = 1; j < n - i; j++) {
+            if(v[j] < v[i + 1]) {
+                int temp = v[j];
+                v[j] = v[i + 1];
+                v[i + 1] = temp;
+                swap = 1;
+            }
+            
+        }
+        
+        if (swap == 0) {
+            break;
+        }
+    }
+    for (int i =0; i < n; i++) {
+        printf("%d ",v[i]);
+    }
+}
+
+void insertionSort(int *v, int n) {
+    for (int i = 1; i < n; i++) {
+        
+        
+        if(v[i] < v[i - 1]) {
+            for(int x = i; x > 0; x--) {
+                if(v[i] > v[x]) {
+                        int temp = v[i];
+                        v[i] = v[x];
+                        v[x] = temp;
+                }
+            }
+        }
+    }
+    for (int i =0; i < n; i++) {
+        printf("%d ",v[i]);
+    }
+}
+
+
+int main()
+{
+    int numeros[7] = {7,4,6,9,8,2,1};
+    printf("|> ");
+    for(int i = 0; i < 7; i++) {
+        printf("%d ",numeros[i]);
+    }
+    
+    printf("\n|> ");
+    selectionSort(numeros, 7);
+    
+    printf("\n| bubble sort > ");
+    bubbleSort(numeros,7);
+    
+    printf("\n| insertion sort > ");
+    insertionSort(numeros,7);
+    return 0;
+}
